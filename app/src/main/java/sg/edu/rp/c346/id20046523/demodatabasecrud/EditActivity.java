9daf796e434dfbaba22697de976b25e2aa01a05b
Zoey-Ang <20046523@myrp.edu.sg>
2021-07-20 21:16:43 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20046523/demodatabasecrud/EditActivity.java
@@ -0,0 +1,62 @@
+package sg.edu.rp.c346.id20046523.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate,btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tvID = findViewById(R.id.tvID);
+        etContent = findViewById(R.id.etContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        Intent i = getIntent();
+        //getSerializableExtra is to link main activity to edit activty.
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+
+                finish();
+                //not recommended to do this because it will be overlapping one activity
+                //over another activity, and pile up. Each activity will take up memory space
+//                Intent i = new Intent(EditActivity.this, MainActivity.class);
+//                startActivity(i);
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+            }
+        });
+
+
+    }
+}
